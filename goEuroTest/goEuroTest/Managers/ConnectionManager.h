58e65818598fa23998ee1e9ffabbf36d0593f52f
Jose Torres <jose.torres@santexgroup.com>
2016-09-27 14:28:36 -0500

    Created connection manager base class.

---
+++ goEuroTest/goEuroTest/Managers/ConnectionManager.h
@@ -0,0 +1,13 @@
+//
+//  ConnectionManager.h
+//  goEuroTest
+//
+//  Created by Santex on 9/27/16.
+//  Copyright © 2016 goeuro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ConnectionManager : NSObject
+
+@end
---
+++ goEuroTest/goEuroTest/Managers/ConnectionManager.m
@@ -0,0 +1,13 @@
+//
+//  ConnectionManager.m
+//  goEuroTest
+//
+//  Created by Santex on 9/27/16.
+//  Copyright © 2016 goeuro. All rights reserved.
+//
+
+#import "ConnectionManager.h"
+
+@implementation ConnectionManager
+
+@end
