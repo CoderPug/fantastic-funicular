eb9e4f88b01bcfa3b22a281933f388ad500a356f
Jose Torres <jose.torres@santexgroup.com>
2016-09-27 15:40:06 -0500

    Created custom table view cell as nib.

---
+++ goEuroTest/goEuroTest/Custom Views/GenericDataTableViewCell.m
@@ -0,0 +1,22 @@
+//
+//  GenericDataTableViewCell.m
+//  goEuroTest
+//
+//  Created by Santex on 9/27/16.
+//  Copyright © 2016 goeuro. All rights reserved.
+//
+
+#import "GenericDataTableViewCell.h"
+
+@implementation GenericDataTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+}
+
+@end
